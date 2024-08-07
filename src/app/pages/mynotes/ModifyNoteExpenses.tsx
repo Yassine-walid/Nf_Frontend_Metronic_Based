1cd9e81a937cf0a285a771f46bf82a9a00a3ed28
Walid <yassine1998walid@gmail.com>
2024-08-07 10:36:58 +0100

    Adding Pagination

---
+++ src/app/pages/mynotes/ModifyNoteExpenses.tsx
@@ -0,0 +1,14 @@
+import React, { FC, useEffect, useState } from "react";
+import { Col, Row } from "react-bootstrap";
+const ModifyNoteExpenses: FC = () => {
+  return (
+    <>
+    <Row>
+      <Col>1 of 3</Col>
+      <Col md="auto">Variable width content</Col>
+    </Row>
+    </>
+  );
+};
+
+export { ModifyNoteExpenses };
